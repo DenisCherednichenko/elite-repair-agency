dfe1c46b548f316afe75a9fa278f7ed81d6b432d
DenisCherednichenko <78148328+DenisCherednichenko@users.noreply.github.com>
2025-08-11 11:11:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Сообщение отправлено! (В реальности подключим сервис для email)');
-    // Здесь можно добавить код для реальной отправки, например, через fetch к API.
-});
